1d37124e6edfb04f9dfeca28ccfbd677237db3e5
Igor <igorgbianchi@gmail.com>
2019-05-12 22:08:40 -0300

    First commit

---
+++ linda.py
@@ -0,0 +1,30 @@
+class Linda:
+    def __init__(self):
+        self.tupleSpace = {}
+    # adds tuple to tuple space
+    def _out(self, thread, author, data):
+        if(self.tupleSpace.get(thread)):
+            self.tupleSpace[thread].append({"author": author, "data": data})
+            return "A thread was updated"
+        else:
+            self.tupleSpace[thread] = []
+            self.tupleSpace[thread].append({"author": author, "data": data})
+            return "A new thread was created"
+    # attemps to match some tuple in tuple space, if a match is found, removes from tuple space
+    def _in(self, thread, author, data):
+        removed = False
+        for x in range(0, self.tupleSpace[thread].__len__()):
+            if(self.tupleSpace[thread][x]["author"] == author and self.tupleSpace[thread][x]["data"] == data):
+               removed = self.tupleSpace[thread][x]
+               self.tupleSpace[thread].pop(x)
+        if(not removed):
+            return "Not removed, invalid tuple"
+        else:
+            return str(removed)
+    # similar to in except that the matched tuple remains in tuple space
+    # the esecuting process suspends if it fails to match a tuple
+    def _rd(self, thread):
+        if(self.tupleSpace.get(thread)):
+            return str(self.tupleSpace[thread])
+        else:
+            return "Failed to return thread"
---
+++ server.py
@@ -0,0 +1,42 @@
+import socket
+import linda
+
+space = linda.Linda()
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        data = data.split()
+        # linda operations
+        if(data[0] == "rd"):
+            messageout = space._rd(data[1])
+            conn.send(messageout.encode())
+        elif(data[0] == "in"):
+            s = ' '
+            messageout = space._in(data[1], data[2], s.join(data[3:data.__len__()]))
+            conn.send(messageout.encode())
+        elif(data[0] == "out"):
+            s = ' '
+            messageout = space._out(data[1], data[2], s.join(data[3:data.__len__()]))
+            conn.send(messageout.encode())
+        else:
+            break
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server_program()
